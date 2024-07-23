4ec3fecd98977e1e2d27a32e13bec83a38d1d821
Carsten Bauer <crstnbr@gmail.com>
2024-07-23 10:28:27 +0200

    run trivial CI on macos and windows

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,23 +6,25 @@ if nthreads() == 1
     @warn("Running test suite with a single thread.")
 end
 
-@testset "ThreadPinningCore.jl" begin
-    @test ispinned() == false
-    @test getaffinity() isa Vector{<:Integer}
-    mask = getaffinity()
-    @test length(mask) >= Sys.CPU_THREADS
-    @test sum(mask) > 0
-    @test isnothing(printmask(mask))
-    @test isnothing(printaffinity())
-    @test getcpuid() isa Integer
-    @test getcpuid() >= 0
+@static if Sys.islinux()
+    @testset "ThreadPinningCore.jl" begin
+        @test ispinned() == false
+        @test getaffinity() isa Vector{<:Integer}
+        mask = getaffinity()
+        @test length(mask) >= Sys.CPU_THREADS
+        @test sum(mask) > 0
+        @test isnothing(printmask(mask))
+        @test isnothing(printaffinity())
+        @test getcpuid() isa Integer
+        @test getcpuid() >= 0
 
-    @test isnothing(pinthread(0))
-    @test ispinned()
+        @test isnothing(pinthread(0))
+        @test ispinned()
 
-    @test isnothing(pinthreads([0]))
+        @test isnothing(pinthreads([0]))
 
-    @test ThreadPinningCore.threadids() isa UnitRange{Int}
+        @test ThreadPinningCore.threadids() isa UnitRange{Int}
 
-    # TODO: Test kwargs
+        # TODO: Test kwargs
+    end
 end
